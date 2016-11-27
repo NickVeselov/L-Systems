70a8bf5c53aafc626aa71ef00e565c42c43341d0
Nikita Veselov <qamelior@gmail.com>
2016-11-27 15:27:17 +0000

    .

---
+++ Source/LSystems/LSystemsGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "LSystemsGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LSYSTEMS_API ALSystemsGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
